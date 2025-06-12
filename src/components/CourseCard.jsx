6969d2ccd62b21709d99b660cf774930b2394b50
Erik Eriksson Broomé <erik.o.eriksson@telia.com>
2025-06-12 19:46:11 +0200

    Created the course cards and courses with course data, created also a course register where you can fill in form and choose a course to register to.

--- src/components/CourseCard.jsx
+++ src/components/CourseCard.jsx
@@ -1,13 +1,17 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+
 export default function CourseCard() {
   return (
-    <>
-      <h2>Course card</h2>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores, sint
-        quibusdam distinctio natus architecto, atque dolores, voluptas quae modi
-        illum delectus dicta dolorem veritatis. Voluptatum et magni minus
-        architecto nulla!
-      </p>
-    </>
+    <div className="card col-6">
+      <div className="card-body">
+        <h2 className="card-title">Course card</h2>
+        <p className="card-text">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores, sint
+          quibusdam distinctio natus architecto, atque dolores, voluptas quae
+          modi illum delectus dicta dolorem veritatis. Voluptatum et magni minus
+          architecto nulla!
+        </p>
+      </div>
+    </div>
   );
 }
--- src/pages/Courses.jsx
+++ src/pages/Courses.jsx
@@ -1,8 +1,18 @@
+import CourseCard from "../components/CourseCard";
+
 export default function Courses() {
   return (
     <div>
       <h2>Heres the Courses page</h2>
       <p>Insert list of courses in card form here.</p>
+      <div className="container row-md row-cols-2 align-items-start ">
+        <div className="row justify-content-around">
+          <CourseCard />
+          <CourseCard />
+          <CourseCard />
+          <CourseCard />
+        </div>
+      </div>
     </div>
   );
 }
